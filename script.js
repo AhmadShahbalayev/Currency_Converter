8efd371a04387b3ebd656cf4d7a66d5951585f37
AhmadShahbalayev <ahmedsahbalayev@gmail.com>
2020-04-13 00:56:51 +0400

    updated

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.onload = function () {
     document.getElementById("default-currency").click();
 };
 
-let rightInput = document.getElementById('right-input');
 let leftInput = document.getElementById('left-input');
+let rightInput = document.getElementById('right-input');
 
 let iHaves = document.querySelectorAll('.i-have');
 let iWants = document.querySelectorAll('.i-want');
@@ -35,16 +35,31 @@ function clicked(e) {
         }
     }
     e.target.classList.add('clicked');
-    calculateRate();
+    calculateRight();
+    calculateLeft();
 
-    leftInput.addEventListener('input', calculateRate);
-    function calculateRate() {
+    leftInput.addEventListener('input', calculateRight);
+    function calculateRight() {
         fetch(`https://api.ratesapi.io/api/latest?base=${clickedIhave}&symbols=${clickedIwant}`)
             .then(res => res.json()
                 .then((res) => {
-                    rightInput.value = leftInput.value * +res.rates[clickedIwant].toFixed(5);
-                    leftDescription.innerText = `1 ${clickedIhave} = ${+res.rates[clickedIwant].toFixed(5)} ${clickedIwant}`
-                    rightDescription.innerText = `1 ${clickedIwant} = ${(1 / +res.rates[clickedIwant]).toFixed(5)} ${clickedIhave}`
+                    rightInput.value = leftInput.value * +res.rates[clickedIwant].toFixed(4);
+                    leftDescription.innerText = `1 ${clickedIhave} = ${+res.rates[clickedIwant].toFixed(4)} ${clickedIwant}`
+                    rightDescription.innerText = `1 ${clickedIwant} = ${(1 / +res.rates[clickedIwant]).toFixed(4)} ${clickedIhave}`
+                },
+                    err => {
+                        console.log(err);
+                    }
+                ));
+    }
+    rightInput.addEventListener('input', calculateLeft);
+    function calculateLeft() {
+        fetch(`https://api.ratesapi.io/api/latest?base=${clickedIwant}&symbols=${clickedIhave}`)
+            .then(res => res.json()
+                .then((res) => {
+                    leftInput.value = rightInput.value * +res.rates[clickedIhave].toFixed(4);
+                    leftDescription.innerText = `1 ${clickedIwant} = ${+res.rates[clickedIhave].toFixed(4)} ${clickedIhave}`
+                    rightDescription.innerText = `1 ${clickedIhave} = ${(1 / +res.rates[clickedIhave]).toFixed(4)} ${clickedIwant}`
                 },
                     err => {
                         console.log(err);
@@ -53,24 +68,29 @@ function clicked(e) {
     }
 }
 
-// let leftDiv = document.getElementById('left-div');
-// let rightDiv = document.getElementById('right-div');
+let leftDiv = document.getElementById('left-div');
+let rightDiv = document.getElementById('right-div');
 
-// let arrowButton = document.getElementById('button');
-// arrowButton.addEventListener('click', () => {
-    // let cloneRight = rightDiv.cloneNode(true);
-    // let cloneLeft = leftDiv.cloneNode(true);
-    // rightDiv.replaceWith(cloneLeft);
-    // leftDiv.replaceWith(cloneRight);
+let left = leftDiv.getBoundingClientRect().left;
+let right = rightDiv.getBoundingClientRect().left;
 
-    // let rectLeftLeft = leftDiv.getBoundingClientRect().left;
-    // let rectRightLeft = rightDiv.getBoundingClientRect().left;
-    // leftDiv.classList.add('absolute');
-    // rightDiv.classList.add('absolute');
-    // leftDiv.style.left = rectRightLeft;
-    // rightDiv.style.left = rectLeftLeft;
-    // leftDiv.classList.remove('absolute');
-    // rightDiv.classList.remove('absolute');
-    // console.log(rectLeftLeft);
-    // console.log(rectRightLeft);
-// })
+let arrowButton = document.getElementById('button');
+arrowButton.addEventListener('click', toggle);
+
+function toggle() {
+    leftDiv.classList.add('absolute');
+    rightDiv.classList.add('absolute');
+    leftDiv.style.right = 0;
+    rightDiv.style.left = 0;
+    leftDiv.style.left = '';
+    arrowButton.removeEventListener('click', toggle);
+    arrowButton.addEventListener('click', reverseToggle);
+}
+
+function reverseToggle() {
+    leftDiv.style.left = 0;
+    rightDiv.style.right = 0;
+    rightDiv.style.left = '';
+    arrowButton.removeEventListener('click', reverseToggle);
+    arrowButton.addEventListener('click', toggle);
+}
