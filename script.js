0a7c534a91e118de30780b4f7dccdf2735a9cdce
AhmadShahbalayev <ahmedsahbalayev@gmail.com>
2020-04-12 22:25:21 +0400

    updated

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.addEventListener('load', calculateRate);
+window.onload = function () {
+    document.getElementById("default-currency").click();
+};
 
 let rightInput = document.getElementById('right-input');
 let leftInput = document.getElementById('left-input');
@@ -6,20 +8,14 @@ let leftInput = document.getElementById('left-input');
 let iHaves = document.querySelectorAll('.i-have');
 let iWants = document.querySelectorAll('.i-want');
 
+let leftDescription = document.getElementById('left-description');
+let rightDescription = document.getElementById('right-description');
+
 let clickedIhave = 'RUB';
 let clickedIwant = 'USD';
 
 iHaves.forEach(item => item.addEventListener('click', clicked));
 iWants.forEach(item => item.addEventListener('click', clicked));
-
-
-leftInput.addEventListener('input', calculateRate);
-
-function calculateRate() {
-    if (clickedIhave === 'RUB' && clickedIwant === 'USD') {
-        rightInput.value = leftInput.value * 1.7025;
-    }
-}
 
 function clicked(e) {
     if (e.target.classList.contains('i-have')) {
@@ -39,7 +35,42 @@ function clicked(e) {
         }
     }
     e.target.classList.add('clicked');
+    calculateRate();
+
+    leftInput.addEventListener('input', calculateRate);
+    function calculateRate() {
+        fetch(`https://api.ratesapi.io/api/latest?base=${clickedIhave}&symbols=${clickedIwant}`)
+            .then(res => res.json()
+                .then((res) => {
+                    rightInput.value = leftInput.value * +res.rates[clickedIwant].toFixed(5);
+                    leftDescription.innerText = `1 ${clickedIhave} = ${+res.rates[clickedIwant].toFixed(5)} ${clickedIwant}`
+                    rightDescription.innerText = `1 ${clickedIwant} = ${(1 / +res.rates[clickedIwant]).toFixed(5)} ${clickedIhave}`
+                },
+                    err => {
+                        console.log(err);
+                    }
+                ));
+    }
 }
 
-let response = fetch('https://api.ratesapi.io/api/latest?base=USD&symbols=RUB')
-response.then(res => res.json()).then(res => console.log(res))
+// let leftDiv = document.getElementById('left-div');
+// let rightDiv = document.getElementById('right-div');
+
+// let arrowButton = document.getElementById('button');
+// arrowButton.addEventListener('click', () => {
+    // let cloneRight = rightDiv.cloneNode(true);
+    // let cloneLeft = leftDiv.cloneNode(true);
+    // rightDiv.replaceWith(cloneLeft);
+    // leftDiv.replaceWith(cloneRight);
+
+    // let rectLeftLeft = leftDiv.getBoundingClientRect().left;
+    // let rectRightLeft = rightDiv.getBoundingClientRect().left;
+    // leftDiv.classList.add('absolute');
+    // rightDiv.classList.add('absolute');
+    // leftDiv.style.left = rectRightLeft;
+    // rightDiv.style.left = rectLeftLeft;
+    // leftDiv.classList.remove('absolute');
+    // rightDiv.classList.remove('absolute');
+    // console.log(rectLeftLeft);
+    // console.log(rectRightLeft);
+// })
